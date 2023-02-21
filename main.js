b6f9be9279e8b5f84fc32aceb09fc1e9f0be9fe9
FlickNBA <flicknba@icloud.com>
2023-02-21 21:56:52 +0100

    Game finished

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 let getComputerChoice = () => {
   let R = Math.random();
-  return R <= 0.333 ? "Rock" : R <= 0.667 ? "Paper" : "Scissors";
+  return R <= 0.3333 ? "Rock" : R <= 0.6667 ? "Paper" : "Scissors";
 };
 
 let playRound = (playerChoice, computerChoice) => {
+  playerChoice = `${playerChoice.substr(0, 1).toUpperCase()}${playerChoice
+    .substr(1)
+    .toLowerCase()}`;
+
+  if (
+    !(
+      playerChoice == "Rock" ||
+      playerChoice == "Paper" ||
+      playerChoice == "Scissors"
+    )
+  ) {
+    return false;
+  }
+
   let Winners = {
     Rock: "Scissors",
     Paper: "Rock",
@@ -12,19 +26,61 @@ let playRound = (playerChoice, computerChoice) => {
 
   let bWords = ["beats", "beat"];
 
-  console.log(
-    `Player chose ${playerChoice}, Computer chose ${computerChoice}.`
-  );
+  //   console.log(
+  //     `Player chose ${playerChoice}, Computer chose ${computerChoice}.`
+  //   );
 
   if (computerChoice == Winners[playerChoice]) {
     let bWord = playerChoice == "Scissors" ? bWords[1] : bWords[0];
-    console.log(`You win! ${playerChoice} ${bWord} ${computerChoice}!`);
+    return `You win! ${playerChoice} ${bWord} ${computerChoice}!`;
   } else if (computerChoice == playerChoice) {
-    console.log(`Tie! Play another round.`);
+    return `You tied! Play another round.`;
   } else {
     let bWord = computerChoice == "Scissors" ? bWords[1] : bWords[0];
-    console.log(`You lose! ${computerChoice} ${bWord} ${playerChoice}.`);
+    return `You lose! ${computerChoice} ${bWord} ${playerChoice}.`;
   }
 };
 
-playRound("Scissors", getComputerChoice());
+let playGame = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    let playerChoice = prompt("What do you choose?");
+    let gameResult = playRound(playerChoice, getComputerChoice());
+    if (gameResult != false) {
+      let magicLetter = gameResult.substr(4, 1);
+      console.log(gameResult);
+      //   console.log(magicLetter);
+      switch (magicLetter) {
+        case "w":
+          //   console.log(`Player won.`);
+          playerScore++;
+          break;
+        case "l":
+          //   console.log(`Player lost.`);
+          computerScore++;
+          break;
+        case "t":
+          //   console.log(`Player tied.`);
+          break;
+      }
+    } else {
+      console.log(`Player input was incorrect.`);
+    }
+  }
+  //  return R <= 0.3333 ? "Rock" : R <= 0.6667 ? "Paper" : "Scissors";
+  let gameWinner =
+    playerScore > computerScore
+      ? "Player"
+      : playerScore < computerScore
+      ? "Computer"
+      : playerScore == computerScore
+      ? "Tie"
+      : "???";
+  console.log(`Final score:
+    Player => ${playerScore}
+    Computer => ${computerScore}
+    Winner => ${gameWinner}`);
+};
+
+playGame();
