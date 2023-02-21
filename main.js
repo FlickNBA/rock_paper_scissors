49b3f66851ee343cedbe80776c3e92f7fe00b065
FlickNBA <flicknba@icloud.com>
2023-02-21 21:33:02 +0100

    Some logic added

--- main.js
+++ main.js
@@ -3,8 +3,28 @@ let getComputerChoice = () => {
   return R <= 0.333 ? "Rock" : R <= 0.667 ? "Paper" : "Scissors";
 };
 
-console.log(getComputerChoice());
-
 let playRound = (playerChoice, computerChoice) => {
-    
-}
+  let Winners = {
+    Rock: "Scissors",
+    Paper: "Rock",
+    Scissors: "Paper",
+  };
+
+  let bWords = ["beats", "beat"];
+
+  console.log(
+    `Player chose ${playerChoice}, Computer chose ${computerChoice}.`
+  );
+
+  if (computerChoice == Winners[playerChoice]) {
+    let bWord = playerChoice == "Scissors" ? bWords[1] : bWords[0];
+    console.log(`You win! ${playerChoice} ${bWord} ${computerChoice}!`);
+  } else if (computerChoice == playerChoice) {
+    console.log(`Tie! Play another round.`);
+  } else {
+    let bWord = computerChoice == "Scissors" ? bWords[1] : bWords[0];
+    console.log(`You lose! ${computerChoice} ${bWord} ${playerChoice}.`);
+  }
+};
+
+playRound("Scissors", getComputerChoice());
